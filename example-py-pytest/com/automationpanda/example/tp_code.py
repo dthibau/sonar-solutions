2e5793b29c405fd4eb259304a32627846cfaef38
David THIBAU <david.thibau@gmail.com>
2024-06-21 09:43:33 +0200

    Ajout projet Python

---
+++ example-py-pytest/com/automationpanda/example/tp_code.py
@@ -0,0 +1,16 @@
+# To REVIEW
+
+def hello_tp():
+    a = 3 * 5
+    print("Hello TP ", a)
+
+
+def another_test(a):
+    print("another test")
+    a = 8 * 10
+    return a
+
+
+def hello_bye(name):
+    print("good bye")
+    return True
